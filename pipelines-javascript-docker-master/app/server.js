829e520e7af44c08e553b8723a90a2f049c81b9c
wjdskagml541 <55969875+wjdskagml541@users.noreply.github.com>
2020-04-02 10:23:40 +0900

    Update server.js

--- pipelines-javascript-docker-master/app/server.js
+++ pipelines-javascript-docker-master/app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world test22\n');
+  res.send('Hello world test\n');
 });
 
 app.listen(PORT, HOST);
